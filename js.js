20bf7cd17e65842cdce96f7088b001f7388f37d6
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-10-06 23:05:50 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,139 +1,158 @@
+
+
 // get joke from api
 let config = { headers: { "Accept": "application/json" } };
 const appendJoke = async function () {
     const res = await axios.get("https://icanhazdadjoke.com/", config);
     const jokeDiv = document.querySelector("#joke");
     jokeDiv.textContent = res.data.joke;
+
 }
 
-const button = document.querySelector("button");
-// algorithm to move gif across screen
-function moveOnClick() {
-    // disable button
-    button.disabled = true;
-    button.classList.remove("button-active")
-    button.classList.add("button-disabled")
 
-    // get image and display it
+function getDistance(x1, y1, x2, y2) {
+    x = x2 - x1;
+    y = y2 - y1;
+    return Math.sqrt(x * x + y * y);
+}
+
+async function moveNew() {
+    // get image
     const img = document.querySelector("#laugh");
-    img.style.display = "block"
+
+    img.style.display = "block";
 
     // get image dimensions
-    const imgWidth = img.getBoundingClientRect().width;
-    const imgHeigth = img.getBoundingClientRect().height;
-    let imgLeft = img.getBoundingClientRect().left;
-    let imgTop = img.getBoundingClientRect().top;
+    const imgWidth = document.querySelector("#laugh").getBoundingClientRect().width;
+    const imgHeigth = document.querySelector("#laugh").getBoundingClientRect().height;
 
     // get window dimensions
     const width = window.innerWidth;
     const height = window.innerHeight;
 
-    // function to retireve random number from an array of numbers
+
+    let imgLeft = img.getBoundingClientRect().left;
+    let imgTop = img.getBoundingClientRect().top;
+
+    console.log(imgLeft, imgTop)
+
+    function randomHeight() {
+        return Math.floor(Math.random() * (height - imgHeigth) + 1);
+    }
+    function randomWidth() {
+        return Math.floor(Math.random() * (width - imgWidth) + 1);
+    }
+
+    // function to retrieve random number from an array of numbers
     function getRandom(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
     }
 
-    // set available pixels to move image
-    let moveArr = [-3, -2, -1, 1, 2, 3]
 
-    // get inital moves
-    let moveX = getRandom(moveArr);
-    let moveY = getRandom(moveArr);
+    let newCoord = [];
+    if (imgLeft == 0) {
+        // img on the left side
+        temp = []
+        temp.push(width - imgWidth);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(height - imgHeigth);
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(0);
+        newCoord.push(temp)
+    }
+    else if (imgLeft == width - imgWidth) {
+        // img on the right side
+        temp = []
+        temp.push(0);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(height - imgHeigth);
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(0);
+        newCoord.push(temp)
+    }
+    else if (imgTop == height - imgHeigth) {
+        // img on the bottom
+        temp = []
+        temp.push(width - imgWidth);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(0);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(0);
+        newCoord.push(temp)
+
+    }
+    else if (imgTop == 0) {
+        // img on the top
+        temp = []
+        temp.push(width - imgWidth);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(0);
+        temp.push(randomHeight());
+        newCoord.push(temp)
+
+        temp = []
+        temp.push(randomWidth());
+        temp.push(height - imgHeigth);
+        newCoord.push(temp)
+    }
+
+    newCoord = getRandom(newCoord)
+
+    newX = newCoord[0]
+    newY = newCoord[1]
+
+    // get distance to current location and max distance
+    let newDistance = getDistance(imgLeft, imgTop, newX, newY);
+    const maxDistance = getDistance(0, 0, width - imgWidth, height - imgHeigth);
+
+    newSpeed = 2000 / maxDistance * newDistance
+
+    img.style.transition = `all ${newSpeed}ms linear`
 
 
-    let move = setInterval(function () {
-        let makeMoveX = false;
-        let makeMoveY = false;
 
-        // x-axis
-        // if move to the right
-        if (moveX > 0) {
-            // if there is space for a whole move -> ok
-            if (imgLeft + moveX + imgWidth <= width) {
-                imgLeft += moveX;
-                makeMoveX = true;
-            }
-            // if there is not space for a whole move -> set to remaining space
-            else if (imgLeft + imgWidth < width) {
-                imgLeft = width - imgWidth
-                makeMoveX = true;
-            }
-            // otherwise don't move
-            else {
-                makeMoveX = false;
-            }
-        }
-        // if move to the left
-        else {
-            // if there is space for a whole move -> ok
-            if (imgLeft + moveX >= 0) {
-                imgLeft += moveX;
-                makeMoveX = true;
-            }
-            // if there is not space for a whole move -> set to remaining space
-            else if (imgLeft > 0) {
-                imgLeft = 0
-                makeMoveX = true;
-            }
-            // otherwise don't move
-            else {
-                makeMoveX = false;
-            }
-        }
-        // y-axis
-        if (moveY > 0) {
-            if (imgTop + moveY + imgHeigth <= height) {
-                imgTop += moveY;
-                makeMoveY = true;
-            }
-            // if there is not space for a whole move -> set to remaining space
-            else if (imgTop + imgHeigth < height) {
-                imgTop = height - imgHeigth
-                makeMoveY = true;
-            }
-            // otherwise don't move
-            else {
-                makeMoveY = false;
-            }
-        }
-        else {
-            // if there is space for a whole move -> ok
-            if (imgTop + moveY >= 0) {
-                imgTop += moveY;
-                makeMoveY = true;
-            }
-            // if there is not space for a whole move -> set to remaining space
-            else if (imgTop > 0) {
-                imgTop = 0
-                makeMoveY = true;
-            }
-            // otherwise don't move
-            else {
-                makeMoveY = false;
-            }
-        }
-        // get new moveX
-        if (makeMoveX === false) {
-            moveX = getRandom(moveArr);
-        }
-        // get new moveY
-        if (makeMoveY === false) {
-            moveY = getRandom(moveArr);
-        }
-        // only move if both directions are okay
-        else if (makeMoveX === true && makeMoveY === true) {
-            img.style.left = imgLeft + "px";
-            img.style.top = imgTop + "px";
-        }
-    }, 10)
+
+    img.style.left = newX + "px";
+    img.style.top = newY + "px";
+
+    setTimeout(() => {
+        moveNew()
+    }, newSpeed + 30);
+
 }
 
-
-// add event (api call)
+const button = document.querySelector("button");
+// add event(api call)
 button.addEventListener("click", appendJoke)
 
 // add event (move image around)
-button.addEventListener("click", moveOnClick)
+button.addEventListener("click", moveNew)
+
+
 
 
 
